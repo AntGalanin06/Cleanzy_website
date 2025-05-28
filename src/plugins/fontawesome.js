d6dbb06356c4e6a60b89c13826d6ea8a5dd33678
Anton <ant_galanin@icloud.com>
2025-05-28 20:09:26 +0300

    Fix import path for Font Awesome icon component to ensure proper functionality

---
+++ src/plugins/fontawesome.js
@@ -0,0 +1,66 @@
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+// Solid icons
+import { 
+  faArrowRight,
+  faArrowDown,
+  faChevronDown,
+  faChevronLeft,
+  faChevronRight,
+  faTimes,
+  faBroom,
+  faUsers,
+  faClock,
+  faLeaf,
+  faShield,
+  faHandHoldingDollar,
+  faPhone,
+  faEnvelope,
+  faCheckCircle,
+  faStar,
+  faPlus,
+  faMinus,
+  faMedal,
+  faHandHoldingHeart,
+  faArrowsLeftRight
+} from '@fortawesome/free-solid-svg-icons'
+
+// Brand icons
+import {
+  faVk,
+  faTelegram,
+  faWhatsapp
+} from '@fortawesome/free-brands-svg-icons'
+
+// Add icons to library
+library.add(
+  // Solid icons
+  faArrowRight,
+  faArrowDown,
+  faChevronDown,
+  faChevronLeft,
+  faChevronRight,
+  faTimes,
+  faBroom,
+  faUsers,
+  faClock,
+  faLeaf,
+  faShield,
+  faHandHoldingDollar,
+  faPhone,
+  faEnvelope,
+  faCheckCircle,
+  faStar,
+  faPlus,
+  faMinus,
+  faMedal,
+  faHandHoldingHeart,
+  faArrowsLeftRight,
+  // Brand icons
+  faVk,
+  faTelegram,
+  faWhatsapp
+)
+
+export { FontAwesomeIcon } 
